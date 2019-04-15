ec50b2a0644e3c85ea9c31a7d590420080882d25
Maxi <maximilianmoser1997@gmail.com>
2019-04-15 21:40:20 +0200

    removed unused files

--- Dockerfile
+++
@@ -1,2 +0,0 @@
-FROM airhacks/glassfish
-COPY ./target/learning.war ${DEPLOYMENT_DIR}
--- buildAndRun.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-mvn clean package && docker build -t test/learning .
-docker rm -f learning || true && docker run -d -p 8080:8080 -p 4848:4848 --name learning test/learning 
